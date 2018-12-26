297f0fd41a37b656881e6a78ee219c72b6f36a98
kebabdestructor <inmain@mail.ru>
2018-12-26 11:00:38 +0300

    4th step

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 import readlineSync from 'readline-sync';
 
-export const userName = () => {
+export const isName = () => {
   const name = readlineSync.question('May I have your name? ');
   return `Hello, ${name}!`;
 };
+
+const makeRandomNumber = () => Math.round(Math.random() * (100 - 1) + 1);
+
+export const isEven = () => {
+  function iter(acc) {
+    if (acc === 3) return `Congratulations, ${isName}!`;
+    const yes = 'yes';
+    const no = 'no';
+    const userAnswer = () => readlineSync.question(`Question: ${makeRandomNumber()}`);
+    if ((makeRandomNumber % 2 === 0 && userAnswer === yes)
+        || (makeRandomNumber % 2 !== 0 && userAnswer === no)) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${userAnswer === yes ? no : yes}`);
+      return `Let's try again, ${isName}!`;
+    }
+    return iter(acc + 1);
+  }
+  return iter(0);
+};
+
+// export { userName, isEven };
