e21aa5b030e82455b050e12d7c83cbaf191b30af
kebabdestructor <inmain@mail.ru>
2018-12-26 20:38:47 +0300

    4th step fixed for review

--- src/index.js
+++ src/index.js
@@ -1,36 +1,34 @@
 import readlineSync from 'readline-sync';
 
-const outputMessage = message => console.log(message);
 const getName = () => {
-  outputMessage('Welcome to the Brain Games!');
-  outputMessage('Answer "yes" if number even otherwise answer "no"');
+  console.log('Welcome to the Brain Games!');
+  console.log('Answer "yes" if number even otherwise answer "no"');
   const name = readlineSync.question('May I have your name? ');
-  outputMessage(`Hello, ${name}!`);
+  console.log(`Hello, ${name}!`);
   return name;
 };
 
 const getRandomNumber = () => Math.round(Math.random() * (100 - 1) + 1);
+
 const isEven = () => {
   const userName = getName();
-  function iter(move) {
-    if (move === 3) return outputMessage(`Congratulations, ${userName}!`);
-    const randomNumber = getRandomNumber();
-    const getUserAnswer = () => readlineSync.question('Your answer: ');
-    const yes = 'yes';
-    const no = 'no';
-    outputMessage(`Question: ${randomNumber}`);
-    const userAnswer = getUserAnswer();
-    if ((randomNumber % 2 === 0 && userAnswer === yes)
-        || (randomNumber % 2 !== 0 && userAnswer === no)) {
-      outputMessage('Correct!');
+  const numberOfSteps = 3;
+  const iter = (move) => {
+    if (move === numberOfSteps) return console.log(`Congratulations, ${userName}!`);
+    const question = getRandomNumber();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
     } else {
-      outputMessage(`${userAnswer} is wrong answer ;(. Correct answer was ${userAnswer === yes
-        ? no : yes}`);
-      return `Let's try again, ${userName}!`;
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${userAnswer === 'yes'
+        ? 'no' : 'yes'}`);
+      return console.log(`Let's try again, ${userName}!`);
     }
     return iter(move + 1);
-  }
-  return iter(0);
+  };
+  iter(0);
 };
 
 export { getName, isEven };
