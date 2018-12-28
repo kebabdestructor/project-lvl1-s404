22a04bcb6c5e4c441b3586823ce7fd01d3973c34
kebabdestructor <inmain@mail.ru>
2018-12-28 11:35:40 +0300

    6th step modified

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,15 +3,15 @@ import getRandomNumber from '../utils';
 import { cons } from 'hexlet-pairs';
 
 const startPhrase = 'Find the greatest common divisor of given numbers.';
+const findGCD = (a, b) => {
+  if (a === 0 || b === 0) return a + b;
+  return findGCD((a > b) ? (a % b) : a, (b > a) ? (b % a) : b);
+};
 
 const gameLogic = () => {
   const num1 = getRandomNumber(50, 2);
   const num2 = getRandomNumber(50, 1);
   const question = `${num1} ${num2}`;
-  const findGCD = (a, b) => {
-    if (a === 0 || b === 0) return a + b;
-    return findGCD((a > b) ? (a % b) : a, (b > a) ? (b % a) : b);
-  };
   const correctAnswer = String(findGCD(num1, num2));
   return cons(question, correctAnswer);
 };
